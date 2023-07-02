650e972a57b583a3369edb3fd25147cb1016e719
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-07-02 20:22:22 +0700

    init topic 1

---
+++ 01-NumberFormatting.js
@@ -0,0 +1,36 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const numbertext = readline();
+
+let numbers = numbertext.match(/\d+(\.?\d+)?/ig)
+ 
+if (!numbers) {
+    console.log(numbertext)
+    return;
+}
+ 
+const str = (numbers.join('') / 2).toString().split('.')
+
+let str1 = str[0].startsWith('0') ? ''.padStart(9, 'x') : str[0].padStart(9, 'x')
+let str2 = str[1] ? str[1].padEnd(6, 'x') : ''.padEnd(6, 'x')
+
+const result1 = [str1[0], ...str1.slice(1)].map((char, index) => {
+    if (index % 3 === 0 && index !== 0) {
+        return ',' + char
+    } else {
+        return char
+    }
+}).join('')
+
+const result2 = [str2[0], ...str2.slice(1)].map((char, index) => {
+    if (index % 3 === 0 && index !== 0) {
+        return '.' + char
+    } else {
+        return char
+    }
+}).join('')
+
+console.log([result1, result2].join('.'))
