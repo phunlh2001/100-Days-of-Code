6163eae8939a86eabd612b34f9df0bdfbb9e9e1e
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-07-07 04:39:07 +0700

    init: day6 topic 3

---
+++ 03-UnitFraction.js
@@ -0,0 +1,20 @@
+const n = parseInt(readline());
+/**
+ * <=> xy = ny + nx
+ * <=> xy - nx = ny
+ * <=> x(y-n) = ny with (x > y)
+ * <=> x = ny / y-n
+ * 
+ * Having n is input, y = n + 1, find x
+ **/
+for (let i=1; i<=n; i++) {
+    let y = n + i
+    let numerator = n*y
+    let denominator = y-n
+    let x = numerator / denominator
+    // only receive 'x' value as an integer
+    if (Number.isInteger(x)) {
+        console.error('i:', i)
+        console.log(`1/${n} = 1/${x} + 1/${y}`)
+    }
+}
