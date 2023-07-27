ae8031e20771f3399b83485640bced86347d8d1f
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-07-27 12:23:59 +0700

    init topic 5

---
+++ 05-ConverCharToNum.js
@@ -0,0 +1,17 @@
+const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const map = {}
+for (let i = 0; i < str.length; i++) {
+  map[str[i]] = i + 1
+}
+
+function convert(str) {
+  let result = ""
+  for (let i = 0; i < str.length; i++) {
+    if (map[str[i]] !== undefined) {
+      result += map[str[i]]
+    }
+  }
+  return result
+}
+
+console.log(convert("ABCD"))
