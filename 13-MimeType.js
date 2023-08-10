68b1a2d2e49f9b7fb097c5d1db234aae98f9e01b
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-08-11 00:05:09 +0700

    hashtable, loops, array

--- 13-MimeType.js
+++ 13-MimeType.js
@@ -1,26 +1,22 @@
-const N = parseInt(readline()); // Number of elements which make up the association table.
-const Q = parseInt(readline()); // Number Q of file names to be analyzed.
+const N = parseInt(readline()) // Number of elements which make up the association table.
+const Q = parseInt(readline()) // Number Q of file names to be analyzed.
 
 const mimeMap = {}
 for (let i = 0; i < N; i++) {
-    var inputs = readline().split(' ');
-    const EXT = inputs[0].toLocaleLowerCase(); // file extension
-    const MT = inputs[1]; // MIME type.
+    var inputs = readline().split(' ')
+    const EXT = inputs[0].toLocaleLowerCase() // file extension
+    const MT = inputs[1] // MIME type.
 
     mimeMap[EXT] = MT
 }
 for (let i = 0; i < Q; i++) {
-    const FNAME = readline(); // One file name per line.
+    const FNAME = readline() // One file name per line.
     const idx = FNAME.lastIndexOf('.')
 
     if (idx !== -1) {
-        const extension = FNAME.substring(idx + 1).toLowerCase();
-        if (mimeMap[extension] !== undefined) {
-            console.log(mimeMap[extension]);
-        } else {
-            console.log('UNKNOWN');
-        }
+        const extension = FNAME.substring(idx + 1).toLowerCase()
+        console.log(mimeMap[extension] ?? 'UNKNOWN')
     } else {
-        console.log('UNKNOWN');
+        console.log('UNKNOWN')
     }
 }
