24d8f1943a785e077fe83273265637e7d58da00b
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-08-11 00:06:53 +0700

    update

--- 12-ParseSqlQueries.js
+++ 12-ParseSqlQueries.js
@@ -1,5 +1,5 @@
 const sqlQuery = readline();
-// console.error(sqlQuery)
+console.error(sqlQuery)
 const ROWS = parseInt(readline());
 const tableHeader = readline();
 
@@ -7,4 +7,26 @@ const tableRows = []
 for (let i = 0; i < ROWS; i++) {
     const tableRow = readline()
     tableRows.push(tableRow)
+}
+
+console.error(tableRows)
+parseQuery(sqlQuery, tableHeader, tableRows)
+
+function parseQuery(query, header, rows) {
+    const found = query.match(/SELECT (.+) FROM/)
+    if (found[1] === "*") {
+        console.log(header)
+        rows.forEach(item => console.log(item))
+    } else {
+        // ignore element of second (.+)
+        const regex = /SELECT (.+) FROM (.+) WHERE (.+)/
+        const [, selectNameStr,, whereStr] = query.match(regex)
+        const selectNames = selectNameStr.split(", ")
+        const whereCondition = whereStr.split(" = ")
+
+        const map = {}
+        for (const item of rows) {
+            
+        }
+    }
 }
