8a22d3149f8f039f54d43ba60047b8733c64aa1b
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-08-10 23:14:54 +0700

    hashtable, loops, array

---
+++ 13-MimeType.js
@@ -0,0 +1,26 @@
+const N = parseInt(readline()); // Number of elements which make up the association table.
+const Q = parseInt(readline()); // Number Q of file names to be analyzed.
+
+const mimeMap = {}
+for (let i = 0; i < N; i++) {
+    var inputs = readline().split(' ');
+    const EXT = inputs[0].toLocaleLowerCase(); // file extension
+    const MT = inputs[1]; // MIME type.
+
+    mimeMap[EXT] = MT
+}
+for (let i = 0; i < Q; i++) {
+    const FNAME = readline(); // One file name per line.
+    const idx = FNAME.lastIndexOf('.')
+
+    if (idx !== -1) {
+        const extension = FNAME.substring(idx + 1).toLowerCase();
+        if (mimeMap[extension] !== undefined) {
+            console.log(mimeMap[extension]);
+        } else {
+            console.log('UNKNOWN');
+        }
+    } else {
+        console.log('UNKNOWN');
+    }
+}
