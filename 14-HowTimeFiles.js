ef0314c9f0cbb808e5b4b2dcb085e5e6bd737ef0
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-08-12 23:59:45 +0700

    new puzzle

---
+++ 14-HowTimeFiles.js
@@ -0,0 +1,77 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const BEGIN = readline();
+const END = readline();
+console.error(BEGIN, END)
+
+console.log(dateTimeFormater(BEGIN, END))
+
+function dateTimeFormater(beginDate, endDate) {
+    const [beginDay, beginMonth, beginYear] = beginDate.split(".").map(Number)
+    const [endDay, endMonth, endYear] = endDate.split(".").map(Number)
+
+    const lastDayInMonth = {}
+    for (let i = 0; i < 12; i++) {
+        const month = i + 1
+        if (month === 2) {
+            lastDayInMonth[month] = 28
+        } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+            lastDayInMonth[month] = 30
+        } else {
+            lastDayInMonth[month] = 31
+        }
+    }
+
+    let yearDiff = endYear - beginYear
+    let monthDiff = endMonth - beginMonth
+    let dayDiff = endDay - beginDay
+
+    
+    if (dayDiff < 0) {
+        monthDiff--
+        dayDiff += lastDayInMonth[beginMonth]
+    }
+    
+    if (monthDiff < 0) {
+        yearDiff--
+        monthDiff += 12
+    }
+    
+    let totalDays = getTotalDaysPerYear(beginYear, yearDiff) + dayDiff
+    
+    for (let i = beginMonth - 1; i < beginMonth + (monthDiff - 1); i++) {
+        let currMonth = ((i + 12) % 12) + 1
+        totalDays += lastDayInMonth[currMonth]
+        console.error('1:', lastDayInMonth[currMonth])
+        
+        if (currMonth === 1 && isLeapYear(beginYear)) {
+            totalDays += 1
+        }
+    }
+
+    let result = []
+    if (yearDiff > 0) {
+        result.push(`${yearDiff} ${yearDiff === 1 ? 'year' : 'years'}`);
+    }
+    if (monthDiff > 0) {
+        result.push(`${monthDiff} ${monthDiff === 1 ? 'month' : 'months'}`)
+    }
+    result.push(`total ${totalDays} ${totalDays === 1 ? 'day' : 'days'}`)
+
+    return result.join(", ")
+}
+
+function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)
+}
+
+function getTotalDaysPerYear(startYear, yearDiff) {
+    let totalDays = 0
+    for (let year = startYear; year < startYear + yearDiff; year++) {
+        totalDays += (isLeapYear(year) ? 366 : 365)
+    }
+    return totalDays
+}
