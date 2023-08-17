3bf9c5a1e5153aaca0cdeef74c6dd473c1b2ec7b
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-08-17 11:55:57 +0700

    solution: hashmap, class, trans object to array

---
+++ 17-HungerGames.js
@@ -0,0 +1,49 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Tribute {
+    constructor(name) {
+        this.name = name
+        this.killed = []
+        this.killer = null
+    }
+    setKill(name) {
+        this.killed.push(name)
+    }
+    setKiller(name) {
+        this.killer = name
+    }
+    getMsg() {
+        const killedSort = this.killed.sort()
+        const killed = this.killed.length === 0 ? "None" : killedSort.join(", ")
+        const killer = this.killer === null ? "Winner" : this.killer
+        return `Name: ${this.name}\nKilled: ${killed}\nKiller: ${killer}`
+    }
+}
+
+const map = {}
+const tributes = parseInt(readline());
+for (let i = 0; i < tributes; i++) {
+    const playerName = readline();
+    map[playerName] = new Tribute(playerName)
+}
+
+const turns = parseInt(readline());
+for (let i = 0; i < turns; i++) {
+    const names = readline().split(" killed ")
+    const killer = names[0]
+    const victims = names[1].split(", ")
+
+    victims.forEach(victim => {
+        map[killer].setKill(victim)
+        map[victim].setKiller(killer)
+    })
+}
+
+const result = []
+for (const key of Object.keys(map).sort()) {
+    result.push(map[key].getMsg());
+}
+
+console.log(result.join("\n\n"))
