1397d06f98689434fa7a120c8a33408a570c15a9
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-08-22 01:41:10 +0700

    fix: passed 2 cases

--- 12-ParseSqlQueries.js
+++ 12-ParseSqlQueries.js
@@ -1,3 +1,7 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
 const sqlQuery = readline();
 console.error(sqlQuery)
 const ROWS = parseInt(readline());
@@ -9,24 +13,93 @@ for (let i = 0; i < ROWS; i++) {
     tableRows.push(tableRow)
 }
 
-console.error(tableRows)
 parseQuery(sqlQuery, tableHeader, tableRows)
 
 function parseQuery(query, header, rows) {
-    const found = query.match(/SELECT (.+) FROM/)
-    if (found[1] === "*") {
-        console.log(header)
-        rows.forEach(item => console.log(item))
-    } else {
-        // ignore element of second (.+)
-        const regex = /SELECT (.+) FROM (.+) WHERE (.+)/
-        const [, selectNameStr,, whereStr] = query.match(regex)
-        const selectNames = selectNameStr.split(", ")
-        const whereCondition = whereStr.split(" = ")
-
-        const map = {}
-        for (const item of rows) {
+    if (query.includes('ORDER BY')) {
+        if (query.includes('WHERE')) {
+            const match = query.match(/SELECT (.+) FROM (.+) WHERE (.+) ORDER BY (.+) DESC/)
+            const selectNames = match[1].split(', ')
+            const [key, value] = match[3].split(' = ')
+            const orderByColumn = match[4]
+    
+            const mapCondition = { [key]: value }
+            const selectedIndices = []
+            header.split(' ').forEach((columnName, index) => {
+                if (selectNames.includes(columnName)) {
+                    selectedIndices.push(index)
+                }
+            })
+    
+            const filteredRows = []
+            rows.forEach(row => {
+                const values = row.split(' ')
+                let satisfiesCondition = true
+    
+                Object.keys(mapCondition).forEach(conditionKey => {
+                    const conditionValue = mapCondition[conditionKey]
+                    const columnIndex = header.split(' ').indexOf(conditionKey)
+    
+                    if (values[columnIndex] !== conditionValue) {
+                        satisfiesCondition = false
+                    }
+                })
+    
+                if (satisfiesCondition) {
+                    const selectedValues = selectedIndices.map(index => values[index])
+                    filteredRows.push({ values: selectedValues, originalRow: row })
+                }
+            })
+    
+            const orderByIndex = header.split(' ').indexOf(orderByColumn)
+            const sortedRows = filteredRows.sort((a, b) => {
+                const aValue = parseFloat(a.values[orderByIndex])
+                const bValue = parseFloat(b.values[orderByIndex])
+                return bValue - aValue // Sort in descending order
+            })
+    
+            console.log(header)
+            sortedRows.forEach(row => {
+                console.log(row.originalRow)
+            })
+        } else {
             
         }
+    } else if (query.includes('*')) {
+        console.log(header)
+        rows.forEach(item => console.log(item))
+    } else if (query.includes('WHERE')) {
+        // ignore element of second (.+)
+        const match = query.match(/SELECT (.+) FROM (.+) WHERE (.+)/)
+        const selectNames = match[1].split(', ')
+        const [key, value] = match[3].split(' = ')
+
+        const mapCondition = { [key]: value }
+        const selectedIndices = []
+        header.split(' ').forEach((columnName, index) => {
+            if (selectNames.includes(columnName)) {
+                selectedIndices.push(index)
+            }
+        })
+
+        console.log(selectNames.join(' '))
+        rows.forEach(row => {
+            const values = row.split(' ')
+            let satisfiesCondition = true
+
+            Object.keys(mapCondition).forEach(conditionKey => {
+                const conditionValue = mapCondition[conditionKey]
+                const columnIndex = header.split(' ').indexOf(conditionKey)
+
+                if (values[columnIndex] !== conditionValue) {
+                    satisfiesCondition = false
+                }
+            })
+
+            if (satisfiesCondition) {
+                const selectedValues = selectedIndices.map(index => values[index])
+                console.log(selectedValues.join(' '))
+            }
+        })
     }
 }
