6505a3e736a4e8294c0db9b4277a299ee9064af5
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-08-28 12:28:09 +0700

    fix: shortest code

--- 23-BinaryImage.js
+++ 23-BinaryImage.js
@@ -1,22 +1,14 @@
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
 const h = parseInt(readline());
 const grid = []
 const format = ['.', 'O']
 
 for (let i = 0; i < h; i++) {
     const row = readline().split(' ').map(Number)
-    const process = row.map((item, index) => format[index % 2].repeat(item)).join('')
-    grid.push(process)
+                    .map((count, index) => format[index % 2].repeat(count)).join('')
+    grid.push(row)
 }
 
 const isEqualLength = grid.every(row => row.length === grid[0].length)
 
-if (isEqualLength) {
-    console.log(grid.join('\n'))
-} else {
-    console.log('INVALID')
-}
+if (isEqualLength) console.log(grid.join('\n'))
+else console.log('INVALID')
