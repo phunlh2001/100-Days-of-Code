afab90d0083a33d8d19d92610736a6dc660e9d73
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-10-08 03:22:08 +0700

    Dynamic programming

--- 70-GoroWantChocolate.js
+++ 70-GoroWantChocolate.js
@@ -1,27 +1,27 @@
 // Medium - https://www.codingame.com/training/medium/goro-want-chocolate
-var [h, w] = readline().split(' ').map(Number);
-let squared;
+var [h, w] = readline().split(' ').map(Number)
+let squared
 
-squared = new Array(h + 1).fill(null).map(() => new Array(w + 1).fill(0));
+squared = new Array(h + 1).fill(null).map(() => new Array(w + 1).fill(0))
 
-console.log(calc(h, w));
+console.log(calc(h, w))
 
 function calc(h, w) {
-  if (h === w) return 1;
+  if (h === w) return 1
 
-  if (squared[h][w] !== 0) return squared[h][w];
+  if (squared[h][w] !== 0) return squared[h][w]
 
-  let horizontal = Number.MAX_SAFE_INTEGER;
-  let vertical = Number.MAX_SAFE_INTEGER;
+  let horizontal = Number.MAX_SAFE_INTEGER
+  let vertical = Number.MAX_SAFE_INTEGER
 
   for (let i = 1; i <= h / 2; i++) {
-    horizontal = Math.min(calc(i, w) + calc(h - i, w), horizontal);
+    horizontal = Math.min(calc(i, w) + calc(h - i, w), horizontal)
   }
 
   for (let i = 1; i <= w / 2; i++) {
-    vertical = Math.min(calc(h, i) + calc(h, w - i), vertical);
+    vertical = Math.min(calc(h, i) + calc(h, w - i), vertical)
   }
 
-  squared[h][w] = Math.min(horizontal, vertical);
-  return squared[h][w];
+  squared[h][w] = Math.min(horizontal, vertical)
+  return squared[h][w]
 }
